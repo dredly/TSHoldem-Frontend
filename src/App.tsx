68f53c06ca7d28d6c8445d35acc9b1839470bfaa
Daniel Redly <dredly@gmail.com>
2023-02-19 15:59:00 +0000

    setup useEffectOnce

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffectOnceWhen } from "rooks";
 import GameSpace from "./components/GameSpace"
 
 const App = () => {
+  
+  useEffectOnceWhen(() => {
+    console.log("using effect")
+    const ws = new WebSocket("ws://localhost:8080")
+    ws.onopen = ev => {
+      console.log("Opened ws connection")
+    }
+  })
+
   return (
     <GameSpace />
   )
