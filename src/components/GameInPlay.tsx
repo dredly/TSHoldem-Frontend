f32762610e4f56970d73fb901242304b471b7010
Daniel Redly <dredly@gmail.com>
2023-02-20 00:41:59 +0000

    Started styling game

--- src/components/GameInPlay.tsx
+++ src/components/GameInPlay.tsx
@@ -2,21 +2,31 @@ import styled from 'styled-components'
 import { Game } from "../types"
 import PlayingCard from './PlayingCard'
 
+const GameContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
 const GameTable = styled.div`
     background: #183A1D;
-    width: 800px;
-    height: 550px;
+    width: 1000px;
+    height: 600px;
 `
 
 const GameInPlay = ({ game }: { game: Game}) => {
     // Temporarily displying 5 cards from deck just to test out styling
     return (
         <>
-            <p>Game in play</p>
-            <p>Money in pot = <strong>${game.pot}</strong></p>
-            <GameTable>
-                {game.deck.slice(0, 5).map(c => <PlayingCard card={c} key={c.rank.toString() + c.suit}/>)}
-            </GameTable>
+            <GameContainer>
+                <p>Money in pot = <strong>${game.pot}</strong></p>
+                <GameTable>
+                    {game.deck.slice(0, 5).map(c => <PlayingCard card={c} key={c.rank.toString() + c.suit}/>)}
+                </GameTable>
+                <h2>DEBUG ZONE</h2>
+                <p>{JSON.stringify(game)}</p>
+            </GameContainer>
         </>
     )
 }
